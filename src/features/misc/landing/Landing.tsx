a6a95bb3880b9e25b375ab9669122647f38d4a6f
AJ5154 <ashishjadhav597@gmail.com>
2024-04-25 12:23:12 +0530

    added link for gym and gives routing

--- src/features/misc/landing/Landing.tsx
+++ src/features/misc/landing/Landing.tsx
@@ -15,6 +15,9 @@ export const Landing = () => {
         <li>
           <Link to="/app/plans">Go to Plans</Link>
         </li>
+        <li>
+          <Link to="/auth/login">Go to Gym</Link>
+        </li>
       </ul>
     </>
   );
